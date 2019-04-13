dc39fd4ed0fbc85ddc9a5b3fc0b7113dd8831397
j86hughes <j86hughes@gmail.com>
2019-04-13 18:28:49 +0100

    finished tutorial 2

--- routes/api/items.js
+++ routes/api/items.js
@@ -15,23 +15,23 @@ router.get('/', (req, res) => {
 });
 
 // @route   POST api/items
-// @desc    Create An Item
-// @access  Private
-// router.post('/', auth, (req, res) => {
-//   const newItem = new Item({
-//     name: req.body.name
-//   });
-//
-//   newItem.save().then(item => res.json(item));
-// });
+// @desc    Post an item to the database
+// @access  Public
+router.post('/', (req, res) => {
+ const newItem = new Item({
+   name: req.body.name
+ });
 
-// @route   DELETE api/items/:id
-// @desc    Delete A Item
-// @access  Private
-// router.delete('/:id', auth, (req, res) => {
-//   Item.findById(req.params.id)
-//     .then(item => item.remove().then(() => res.json({ success: true })))
-//     .catch(err => res.status(404).json({ success: false }));
-// });
+ newItem.save().then(item => res.json(item))
+});
+
+// @route   DELETE api/items
+// @desc    Delete an item from the database
+// @access  Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }))
+});
 
 module.exports = router;
